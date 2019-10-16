27cb76ab6b916403d977aabd4a2fd7dee5c6f4cf
Sattvik <sattviksahai24@gmail.com>
2019-10-16 19:52:42 -0400

    added robustness for scalability

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 A numpy implementation of K-Means Clustering
 
 Clustering in action
-![](animation.gif)
+
+![](10ClassAnimation.gif)
+![](3ClassAnimation.gif)
--- kMeans.py
+++ kMeans.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 import random
 
 # Define Parameters
-N = 300
-K = 3
-epochs = 20
+N = 500
+K = 10
+epochs = 30
 
 # Generate n random points
 points = np.random.random_sample((N,2))
@@ -26,6 +26,9 @@ for i in range(1, K):
 	c=np.append(c,points[tempIndices].reshape((1,2)), axis=0)
 print("Centroid Initialization Complete")
 
+# generate colors
+colors = np.random.rand(3,K)
+
 for e in range(epochs):
 	segmentedPoints = [np.empty((0,2)) for i in range(K)]
 	for j in range(N):
@@ -34,21 +37,17 @@ for e in range(epochs):
 		segmentedPoints[np.argmin(centroidDist)] = np.append(segmentedPoints[np.argmin(centroidDist)],points[j].reshape(1,2), axis=0)
 	# Recompute centroids
 	for k in range(K):
-		c[k] = segmentedPoints[k].mean(axis=0)
+		if len(segmentedPoints[k])>0:
+			c[k] = segmentedPoints[k].mean(axis=0)
 
 	# plot points
-	colors = "bgcmykw"
-	#plt.clear()
-	print("segmentedPoints: ", segmentedPoints[0].mean(), segmentedPoints[1].mean(), segmentedPoints[2].mean())
 	for p in range(K):
-		plt.scatter(segmentedPoints[p][:,0],segmentedPoints[p][:,1],c=colors[p])
+		plt.scatter(segmentedPoints[p][:,0],segmentedPoints[p][:,1],c=colors[:,p], marker='o')
 	# plt.scatter(segmentedPoints[1][:,0],segmentedPoints[1][:,1],c='y')
 	# plt.scatter(segmentedPoints[2][:,0],segmentedPoints[2][:,1],c='g')
 	removable = plt.scatter(c[:,0], c[:,1], c='r', marker='^')
 	plt.show(block=False)
-	plt.pause(0.2)
+	plt.pause(0.1)
 	plt.savefig('plots/plot_'+str(e)+'.png')
 	removable.remove()
-
-print("segmentedPoints: ", segmentedPoints[0][:,0], len(segmentedPoints[1]), len(segmentedPoints[2]))
-
+print("Done")
